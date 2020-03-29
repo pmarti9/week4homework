3d7b7cf21c9d1f57718e5404982e8c4b0b961cbc
pmarti99 <parkerjmartin883@gmail.com>
2020-03-28 22:31:08 -0600

    quiz is functional, just need the scores to save and a timer as well.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function(){
+    // Functions
     function buildQuiz(){
       // variable to store the HTML output
       const output = [];
@@ -25,8 +26,10 @@
   
           // add this question and its answers to the output
           output.push(
-            `<div class="question"> ${currentQuestion.question} </div>
-            <div class="answers"> ${answers.join('')} </div>`
+            `<div class="slide">
+              <div class="question"> ${currentQuestion.question} </div>
+              <div class="answers"> ${answers.join("")} </div>
+            </div>`
           );
         }
       );
@@ -70,44 +73,85 @@
       resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
     }
   
+    function showSlide(n) {
+      slides[currentSlide].classList.remove('active-slide');
+      slides[n].classList.add('active-slide');
+      currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block';
+      }
+      if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+      }
+      else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+      }
+    }
+  
+    function showNextSlide() {
+      showSlide(currentSlide + 1);
+    }
+  
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
+  
+    // Variables
     const quizContainer = document.getElementById('quiz');
     const resultsContainer = document.getElementById('results');
     const submitButton = document.getElementById('submit');
     const myQuestions = [
-      {
-        question: "What is the Utes main primary color?",
-        answers: {
-          a: "Red",
-          b: "White",
-          c: "Black",
-          d: "Gray"
+        {
+          question: "What is the Utes main primary color?",
+          answers: {
+            a: "Red",
+            b: "White",
+            c: "Black",
+            d: "Gray"
+          },
+          correctAnswer: "a"
         },
-        correctAnswer: "a"
-      },
-      {
-        question: "Who has the most NBA titles?",
-        answers: {
-          a: "Lakers",
-          b: "Celtics",
-          c: "Sixers",
-          d: "Warriors"
+        {
+          question: "Who has the most NBA titles?",
+          answers: {
+            a: "Lakers",
+            b: "Celtics",
+            c: "Sixers",
+            d: "Warriors"
+          },
+          correctAnswer: "b"
         },
-        correctAnswer: "b"
-      },
-      {
-        question: "How many planets are in the solar system?",
-        answers: {
-          a: "7",
-          b: "10",
-          c: "8",
-          d: "9"
-        },
-        correctAnswer: "c"
-      }
-    ];
+        {
+          question: "How many planets are in the solar system?",
+          answers: {
+            a: "7",
+            b: "10",
+            c: "8",
+            d: "9"
+          },
+          correctAnswer: "c"
+        }
+      ];
+  
     // Kick things off
     buildQuiz();
   
+    // Pagination
+    const previousButton = document.getElementById("previous");
+    const nextButton = document.getElementById("next");
+    const slides = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+  
+    // Show the first slide
+    showSlide(currentSlide);
+  
     // Event listeners
     submitButton.addEventListener('click', showResults);
+    previousButton.addEventListener("click", showPreviousSlide);
+    nextButton.addEventListener("click", showNextSlide);
   })();
