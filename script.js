ab496e6d32e996de2247d41e8d40598ef58ecc94
pmarti99 <parkerjmartin883@gmail.com>
2020-03-28 21:21:58 -0600

    added additional HTML/CSS and Javascript

---
+++ script.js
@@ -0,0 +1,39 @@
+const quizContainer = document.getElementById("quiz");
+const resultsContainer = document.getElementById("results");
+const submitButton = document.getElementById("submit");
+const myQuestions = [
+    {
+      question: "What is the Utes" main primary color?",
+      answers: {
+        a: "Red",
+        b: "White",
+        c: "Black",
+        d: "Gray"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "Who has the most NBA titles?",
+      answers: {
+        a: "Lakers",
+        b: "Celtics",
+        c: "Sixers",
+        d: "Warriors"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: "How many planets are in the solar system?",
+      answers: {
+        a: "7",
+        b: "10",
+        c: "8",
+        d: "9"
+      },
+      correctAnswer: "c"
+    }
+  ];
+
+function buildQuiz(){
+
+};
