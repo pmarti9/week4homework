0f05354b9929a61c85d4c4ab4c8d319a6e3c53fa
pmarti99 <parkerjmartin883@gmail.com>
2020-05-08 22:16:27 -0600

    code quiz rework

--- script.js
+++ script.js
@@ -1,157 +1,160 @@
-(function(){
-    // Functions
-    function buildQuiz(){
-      // variable to store the HTML output
-      const output = [];
+const startEl = document.querySelector('#beginQuiz');
+const questionsTitleEl = document.querySelector('#question-title');
+const choicesEl = document.querySelector('#choices');
+const startScreenEl = document.querySelector('#start-screen');
+const questionsEl = document.querySelector('#questions');
+const timerEl = document.querySelector('#timer')
+const feedbackEl = document.querySelector('#feedback');
+const endScreenEl = document.querySelector('#end-screen');
+const finalScoreEl = document.querySelector('#final-score');
+const initialsEl = document.querySelector('#initials');
+const submitBtn = document.querySelector('#submit');
+const highscoreEl = document.querySelector('#highscore-screen');
+const restartEl = document.querySelector('#restart');
+
+let currentQuestionIndex = 0;
+let time = 120;
+const questions = [
+  {
+    question: 'How many planets are there in the solar system',
+    choices: [6,7,8,9],
+    answer: 8
+  },
+  {
+    question: 'What conference do the Utes play in?',
+    choices: ['Pac-12', 'Mountain West', 'Big 12', 'WAC'],
+    answer: 'Pac-12'
+  },
+  {
+    question: 'What year was Mountain dew introduced?',
+    choices: ['1940', '1948', '1964', '1992'],
+    answer: '1940'
+  },
+]
+
+let timerId;
+
+
+const getQuestion = () => {
+  let currentQuestion = questions[currentQuestionIndex]
+
+  questionsTitleEl.textContent = currentQuestion.question;
+  choicesEl.innerHTML = '';
   
-      // for each question...
-      myQuestions.forEach(
-        (currentQuestion, questionNumber) => {
-  
-          // variable to store the list of possible answers
-          const answers = [];
-  
-          // and for each available answer...
-          for(letter in currentQuestion.answers){
-  
-            // ...add an HTML radio button
-            answers.push(
-              `<label>
-                <input type="radio" name="question${questionNumber}" value="${letter}">
-                ${letter} :
-                ${currentQuestion.answers[letter]}
-              </label>`
-            );
-          }
-  
-          // add this question and its answers to the output
-          output.push(
-            `<div class="slide">
-              <div class="question"> ${currentQuestion.question} </div>
-              <div class="answers"> ${answers.join("")} </div>
-            </div>`
-          );
-        }
-      );
-  
-      // finally combine our output list into one string of HTML and put it on the page
-      quizContainer.innerHTML = output.join('');
+  currentQuestion.choices.forEach(choice => {
+    let choiceButton = document.createElement('button');
+    choiceButton.setAttribute('value', choice);
+    choiceButton.textContent = choice;
+    choiceButton.addEventListener('click', answerCheck);
+    choicesEl.appendChild(choiceButton);
+
+  })
+}
+
+const answerCheck = (event) => {
+  if(event.target.value !== questions[currentQuestionIndex].answer){
+    time -= 15;
+
+    if(time < 0) {
+      time=0;
     }
+    feedbackEl.textContent = 'Wrong!'
+  } else {
+    feedbackEl.textContent = 'Correct!'
+  } 
+
+  feedbackEl.removeAttribute('class');
+  setTimeout(() => {
+    feedbackEl.setAttribute('class', 'hide');
+  }, 2000)
+
+  currentQuestionIndex++;
+
+  if(currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+};
+
+const clockTick = () => {
+  time--;
+  timerEl.textContent = time;
+
+  if(time <=0) {
+    quizEnd();
+  }
+};
+
+const saveHighScore = (event)=>{
+  event.preventDefault();
+  let initials = initialsEl.value.trim();
+
+  if(initials !== ""){
+    let highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+    let newScore = {
+      score: time,
+      initials: initials
+    };
+
+    highscores.push(newScore);
+    localStorage.setItem('highscores', JSON.stringify(highscores));
+
+    printHighScore();
+    // window.location.href = 'highscores.html';
+  } 
+};
+
+const printHighScore = () => {
+  endScreenEl.setAttribute('class','hide')
   
-    function showResults(){
-  
-      // gather answer containers from our quiz
-      const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-      // keep track of user's answers
-      let numCorrect = 0;
-  
-      // for each question...
-      myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-        // find selected answer
-        const answerContainer = answerContainers[questionNumber];
-        const selector = `input[name=question${questionNumber}]:checked`;
-        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-        // if answer is correct
-        if(userAnswer === currentQuestion.correctAnswer){
-          // add to the number of correct answers
-          numCorrect++;
-  
-          // color the answers green
-          answerContainers[questionNumber].style.color = 'lightgreen';
-        }
-        // if answer is wrong or blank
-        else{
-          // color the answers red
-          answerContainers[questionNumber].style.color = 'red';
-        }
-      });
-  
-      // show number of correct answers out of total
-      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-    }
-  
-    function showSlide(n) {
-      slides[currentSlide].classList.remove('active-slide');
-      slides[n].classList.add('active-slide');
-      currentSlide = n;
-      if(currentSlide === 0){
-        previousButton.style.display = 'none';
-      }
-      else{
-        previousButton.style.display = 'inline-block';
-      }
-      if(currentSlide === slides.length-1){
-        nextButton.style.display = 'none';
-        submitButton.style.display = 'inline-block';
-      }
-      else{
-        nextButton.style.display = 'inline-block';
-        submitButton.style.display = 'none';
-      }
-    }
-  
-    function showNextSlide() {
-      showSlide(currentSlide + 1);
-    }
-  
-    function showPreviousSlide() {
-      showSlide(currentSlide - 1);
-    }
-  
-    // Variables
-    const quizContainer = document.getElementById('quiz');
-    const resultsContainer = document.getElementById('results');
-    const submitButton = document.getElementById('submit');
-    const myQuestions = [
-        {
-          question: "What is the Utes main primary color?",
-          answers: {
-            a: "Red",
-            b: "White",
-            c: "Black",
-            d: "Gray"
-          },
-          correctAnswer: "a"
-        },
-        {
-          question: "Who has the most NBA titles?",
-          answers: {
-            a: "Lakers",
-            b: "Celtics",
-            c: "Sixers",
-            d: "Warriors"
-          },
-          correctAnswer: "b"
-        },
-        {
-          question: "How many planets are in the solar system?",
-          answers: {
-            a: "7",
-            b: "10",
-            c: "8",
-            d: "9"
-          },
-          correctAnswer: "c"
-        }
-      ];
-  
-    // Kick things off
-    buildQuiz();
-  
-    // Pagination
-    const previousButton = document.getElementById("previous");
-    const nextButton = document.getElementById("next");
-    const slides = document.querySelectorAll(".slide");
-    let currentSlide = 0;
-  
-    // Show the first slide
-    showSlide(currentSlide);
-  
-    // Event listeners
-    submitButton.addEventListener('click', showResults);
-    previousButton.addEventListener("click", showPreviousSlide);
-    nextButton.addEventListener("click", showNextSlide);
-  })();
+
+  let highscores = JSON.parse(localStorage.getItem('highscores')) || [];
+
+  highscores.sort((a, b) => b.score - a.score)
+
+  highscores.forEach(score => {
+    let liTag = document.createElement('li')
+    liTag.textContent = `${score.initials} - ${score.score}`;
+
+    const olEl = document.querySelector('#highscores');
+    olEl.appendChild(liTag);
+
+  })
+  highscoreEl.removeAttribute('class')
+}
+const quizEnd = () => {
+  clearInterval(timerId);
+
+  endScreenEl.removeAttribute('class');
+
+  finalScoreEl.textContent = time;
+
+  questionsEl.setAttribute('class','hide');
+};
+
+const startQuiz = ()=>{
+  currentQuestionIndex = 0;
+  time = 120;
+  highscoreEl.setAttribute('class','hide')
+
+  startScreenEl.setAttribute('class', 'hide')
+  questionsEl.removeAttribute('class')
+
+  timerId = setInterval(clockTick, 1000)
+
+
+  getQuestion();
+
+};
+
+const checkForEnter = event => {
+  if(event.key === 'Enter'){
+    saveHighScore();
+  }
+};
+
+submitBtn.addEventListener('click', saveHighScore)
+restartEl.addEventListener('click', startQuiz);
+startEl.addEventListener('click', startQuiz);
